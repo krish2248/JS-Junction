468232bcc228e2d33491f5377ca0e1074b5b37e9
Krish Soni <143406295+krish2248@users.noreply.github.com>
2025-02-19 16:19:15 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,40 +12,38 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: '/images/project-1.jpg',
-    title: 'Full stack music app',
-    tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://musify-5al0.onrender.com/'
-  },
-  {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://pixstock-official.vercel.app/'
-  },
-  {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
+    
+    title: 'Dice-App',
+    tags: ['Lost your dice? No worries we\'ve got you covered! 🎲', 'API'],
     projectLink: ''
   },
   {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
+
+    title: 'Movie Library',
+    tags: ['A platform for Cinephiles to review/organise their favorites', 'HTML-CSS-JavaScript'],
+    projectLink: ''
   },
   {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+   
+    title: 'Expense/Progress Tracker',
+    tags: ['Personalized expense/progress tracker with cloud storage', 'HTML-CSS-JavaScript'],
+    projectLink: ''
   },
   {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+
+    title: 'Calorie-BMI Calculator',
+    tags: ['Web-design', 'HTML-CSS-JavaScript'],
+    projectLink: ''
+  },
+  {
+    title: 'Snake-Game',
+    tags: ['Class snakes game', 'HTML-CSS-JavaScript'],
+    projectLink: ''
+  },
+  {
+    title: 'Book Library',
+    tags: ['A platform for readers to review/organize their Favorites.', 'HTML-CSS-JavaScript'],
+    projectLink: ''
   },
 ];
 
@@ -59,7 +57,7 @@ const Work = () => {
       <div className="container">
 
         <h2 className="headline-2 mb-8 reveal-up">
-          My portfolio highlights
+          My Work
         </h2>
 
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
